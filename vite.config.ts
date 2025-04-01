e1d96cac1984e51cd306f590a8e813a88d42a2bc
igor <igalvesreis2121@gmail.com>
2025-04-01 17:21:13 -0300

    mudanças no .json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/arquitetura/',  // Adicione o nome do seu repositório GitHub
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     tailwindcss(),
   ],
